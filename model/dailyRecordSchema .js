da1a256637a8a96673f0bc6097492e1a1c4649e5
unknown <nikhil>
2024-03-06 22:08:03 +0530

    update

--- model/dailyRecordSchema .js
+++ model/dailyRecordSchema .js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dailyRecordSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    required: true
-  },
-  collection: {
-    HDPE: { type: Number, default: 0 },
-    LDPE: { type: Number, default: 0 },
-    PET: { type: Number, default: 0 },
-    PP: { type: Number, default: 0 },
-    PS: { type: Number, default: 0 },
-    PVC: { type: Number, default: 0 },
-    Others: { type: Number, default: 0 }
-  }
+  date: { type: Date, required: true },
+  HDPE: { type: Number, default: 0 },
+  LDPE: { type: Number, default: 0 },
+  PET: { type: Number, default: 0 },
+  PP: { type: Number, default: 0 },
+  PS: { type: Number, default: 0 },
+  PVC: { type: Number, default: 0 },
+  Others: { type: Number, default: 0 },
 });
 
-const DailyRecord = mongoose.model('DailyRecord', dailyRecordSchema);
+const DailyRecord = mongoose.model("DailyRecord", dailyRecordSchema);
 
 module.exports = DailyRecord;
